0e1e8971d52d909577c43fbd34c8f525bd6b8b79
darrentimotius <timotiusdarren@gmail.com>
2025-11-25 09:12:24 +0700

    Revert "Add loading page"
    This reverts commit f2c97842ce98db2eff12befdab8860a97111180a.

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Projects from './components/Projects'
 import About from './components/About'
-import Loading from './components/Loading'
 
 export default function App(){
-  const [isLoaded, setIsLoaded] = useState(false)
-
-  useEffect(() => {
-    function handleLoad(){
-      setIsLoaded(true)
-    }
-
-    // If document already loaded (fast cache/dev), mark as loaded
-    if (document.readyState === 'complete'){
-      setIsLoaded(true)
-      return
-    }
-
-    window.addEventListener('load', handleLoad)
-    return () => window.removeEventListener('load', handleLoad)
-  }, [])
-
-  if (!isLoaded) return <Loading />
-
   return (
     <div className="flex flex-col">
       <header className="h-screen flex flex-col">
