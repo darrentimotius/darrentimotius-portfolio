1adf26d9baa255b1b7e04b31068ac69497a75a0c
darrentimotius <timotiusdarren@gmail.com>
2025-11-26 08:39:04 +0700

    Make scroll to 0,0

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useEffect} from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Projects from './components/Projects'
 import About from './components/About'
 
 export default function App(){
+    useEffect(() => {
+    if (typeof window === 'undefined') return
+    // Matikan auto scroll restore dan paksa ke atas
+    if ('scrollRestoration' in window.history) {
+      window.history.scrollRestoration = 'manual'
+    }
+    window.scrollTo(0, 0)
+  }, [])
+
   return (
     <div className="flex flex-col">
       <header className="h-screen flex flex-col">
