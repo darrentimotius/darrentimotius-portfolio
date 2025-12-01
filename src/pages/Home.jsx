9a735623b3230fd86b682e18d13f23502bf30810
darrentimotius <timotiusdarren@gmail.com>
2025-12-01 15:30:48 +0700

    Add animation on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import { motion } from 'framer-motion'
 
+let isFirstLoad = true;
+
 const Home = () => {
+    const navigationEntries = window.performance.getEntriesByType('navigation');
+    const isReload = navigationEntries.length > 0 && navigationEntries[0].type === 'reload';
+    const shouldAnimate = !isReload || !isFirstLoad;
+    
+    useEffect(() => {
+        isFirstLoad = false;
+    }, [])
     return (
         <div>
             <Navbar/>
-            <motion.div initial={{opacity: 0, y: 50 }} animate={{opacity: 1 ,y: 0 }} transition={{ duration: 0.5 }} className='min-h-screen flex flex-col'>
+            <motion.div initial={shouldAnimate ? {opacity: 0, y: 50 } : false} animate={{opacity: 1 ,y: 0 }} transition={{ duration: 0.5 }} className='min-h-screen flex flex-col'>
                 <Hero/>
             </motion.div>
         </div>
